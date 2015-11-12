da0dc713c5ada08954ddf8a3cf9626b8f8d6b526
kinba <kinba@sina.com>
2015-11-12 23:21:32 +0800

    更正Windows脚本

--- 0_CreateEmptyProject.bat
+++ 0_CreateEmptyProject.bat
@@ -1,8 +1,8 @@
 SET URHO3D_HOME=%CD%\..\Urho3D-1.5
-xcopy /Y /E %URHO3D_HOME%\Android .
-xcopy /Y /E %URHO3D_HOME%\bin .
-xcopy /Y /E %URHO3D_HOME%\CMake .
-xcopy /Y /E %URHO3D_HOME%\Docs .
+xcopy /Y /E %URHO3D_HOME%\Android Android\
+xcopy /Y /E %URHO3D_HOME%\bin bin\
+xcopy /Y /E %URHO3D_HOME%\CMake CMake\
+xcopy /Y /E %URHO3D_HOME%\Docs Docs\
 
 copy %URHO3D_HOME%\*.sh .
 copy %URHO3D_HOME%\*.bat .
@@ -13,7 +13,8 @@ copy %URHO3D_HOME%\License.txt .
 
 xcopy /Y /E Templates\* .
 
-xcopy /Y /E Templates\build_* %URHO3D_HOME%\
-
-
+xcopy /Y /E Templates\build_and\* %URHO3D_HOME%\build_and\
+xcopy /Y /E Templates\build_ios\* %URHO3D_HOME%\build_ios\
+xcopy /Y /E Templates\build_macosx\* %URHO3D_HOME%\build_macosx\
+xcopy /Y /E Templates\build_vs2013\* %URHO3D_HOME%\build_vs2013\
 
