141b9a81102feef248813a34f476d93f22f550a0
kinba <kinba@sina.com>
2015-11-12 23:02:08 +0800

    增加生成调试模式版本的脚本

--- Templates/build_macosx/_genproject_debug.sh
+++ Templates/build_macosx/_genproject_debug.sh
@@ -2,4 +2,4 @@
 #../cmake_macosx.sh ./  -DURHO3D_LUAJIT=1 -DURHO3D_MCPE=1 -DURHO3D_NETWORK=0 -DHOPJOY=1 -DHOPJOY_REMOVE_URHO3D_LUA=1
 export URHO3D_HOME=$(pwd)/../../Urho3D-1.5/build_macosx
 rm CMakeCache.txt
-../cmake_macosx.sh ./  -DCMAKE_BUILD_TYPE=Release
+../cmake_macosx.sh ./  -DCMAKE_BUILD_TYPE=Debug
